ca6021b1de9b31f88408c4466a16d3e2ed9adcac
mbyram <10727907+mbyram@users.noreply.github.com>
2023-10-13 10:59:55 -0400

    Update two_sum.py with solution

--- src/1_Two_Sum/two_sum.py
+++ src/1_Two_Sum/two_sum.py
@@ -1,2 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {} 
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in seen:
+                return [i, seen[diff]]
+            else:
+                seen[nums[i]] = i
